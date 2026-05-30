dde3f0e5b7bcdcb28e202003d3e0e2126fe81dca
ModSault <modsault@gmail.com>
2026-05-30 14:39:53 -0400

    Moved reused functions/variables to here
    - SVG variables
    - Update header to include VsMode intro Camera File
    - Added General DOM adding function
    - Added function to get current focused element and a way to refocus on it (when removing and adding a lot of DOMs)

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ var tooltipDiv = null;
 var GameVersion = 0; // 0 = USA, 1 = Japan, 2 = PAL
 var allGameModeSelectors = [];
 
+// SVG icons I may need (all from https://fonts.google.com/icons)
+const eyeInvisibleSVG = '<svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="var(--font-color)"><path d="m644-428-58-58q9-47-27-88t-93-32l-58-58q17-8 34.5-12t37.5-4q75 0 127.5 52.5T660-500q0 20-4 37.5T644-428Zm128 126-58-56q38-29 67.5-63.5T832-500q-50-101-143.5-160.5T480-720q-29 0-57 4t-55 12l-62-62q41-17 84-25.5t90-8.5q151 0 269 83.5T920-500q-23 59-60.5 109.5T772-302Zm20 246L624-222q-35 11-70.5 16.5T480-200q-151 0-269-83.5T40-500q21-53 53-98.5t73-81.5L56-792l56-56 736 736-56 56ZM222-624q-29 26-53 57t-41 67q50 101 143.5 160.5T480-280q20 0 39-2.5t39-5.5l-36-38q-11 3-21 4.5t-21 1.5q-75 0-127.5-52.5T300-500q0-11 1.5-21t4.5-21l-84-82Zm319 93Zm-151 75Z"/></svg>'
+const eyeVisibleSVG = '<svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="var(--font-color)"><path d="M607.5-372.5Q660-425 660-500t-52.5-127.5Q555-680 480-680t-127.5 52.5Q300-575 300-500t52.5 127.5Q405-320 480-320t127.5-52.5Zm-204-51Q372-455 372-500t31.5-76.5Q435-608 480-608t76.5 31.5Q588-545 588-500t-31.5 76.5Q525-392 480-392t-76.5-31.5ZM214-281.5Q94-363 40-500q54-137 174-218.5T480-800q146 0 266 81.5T920-500q-54 137-174 218.5T480-200q-146 0-266-81.5ZM480-500Zm207.5 160.5Q782-399 832-500q-50-101-144.5-160.5T480-720q-113 0-207.5 59.5T128-500q50 101 144.5 160.5T480-280q113 0 207.5-59.5Z"/></svg>'
+const downloadSVG = '<svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="var(--font-color)"><path d="M480-320 280-520l56-58 104 104v-326h80v326l104-104 56 58-200 200ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z"/></svg>'
+const uploadSVG = '<svg xmlns="http://www.w3.org/2000/svg" height="80px" viewBox="0 -960 960 960" width="80px" fill="var(--font-color)"><path d="M450-313v-371L330-564l-43-43 193-193 193 193-43 43-120-120v371h-60ZM220-160q-24 0-42-18t-18-42v-143h60v143h520v-143h60v143q0 24-18 42t-42 18H220Z"/></svg>'
+const arrowDownwardSVG = '<svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="var(--font-color)"><path d="M440-800v487L216-537l-56 57 320 320 320-320-56-57-224 224v-487h-80Z"/></svg>'
+const XSVG = '<svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="var(--red-button)"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>';
+
 // ---------------------------- Load Information from Local Storage  --------------------------
 
 function loadInfo() {
@@ -113,6 +121,10 @@ function makeNavBar(pathToRoot) {
                 {
                     "text": "Collision Files (hit_pack_xx.pac)",
                     "link": pathToRoot + "/CollisionFiles/"
+                },
+                {
+                    "text": "VsMode Intro Camera Files (ts_cammotxxy.bin)",
+                    "link": pathToRoot + "/VSModeIntro/"
                 }
             ]
         },
@@ -274,6 +286,41 @@ function makeHeader(title, funcToCall = null, makeGameModeSelector = true, makeA
     // add header to page
     let scriptTag = document.currentScript;
     scriptTag.parentNode.replaceChild(topLevel, scriptTag);
+}
+
+// --------------- All in one DOM creation -----------------
+
+function DOM_addAny(domtype, jsonExtras) {
+    let toReturn = document.createElement(domtype);
+    for (const key in jsonExtras) {
+        toReturn[key] = jsonExtras[key];
+    }
+    return toReturn;
+}
+
+// ---------------- If I remove and add elements (like what I usually do) these can be used to refocus on the right element ------------ */
+
+// thanks Claude for both
+function getFocusedElementPath() {
+    let el = document.activeElement;
+    const path = [];
+    while (el && el.nodeType === Node.ELEMENT_NODE) {
+        let selector = el.tagName.toLowerCase();
+        if (el.id) {
+            selector += `#${el.id}`;
+        } else {
+            const index = [...el.parentNode.children].indexOf(el) + 1;
+            selector += `:nth-child(${index})`;
+            if (el.className) selector += `.${[...el.classList].join(".")}`;
+        }
+        path.unshift(selector);
+        el = el.parentNode;
+    }
+    return path.join(" > ");
+}
+function focusOnElement(selector) {
+  const el = document.querySelector(selector);
+  if (el) el.focus({ preventScroll: true });
 }
 
 // --------------- Security related things ------------------
